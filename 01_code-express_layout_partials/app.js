89d852a02f33394ace52da5cb2212cccd776470f
marco-ih-bcn <marco.santonastasi@ironhack.com>
2021-06-24 10:37:16 +0200

    WIP step 1 create templates

--- 01_code-express_layout_partials/app.js
+++ 01_code-express_layout_partials/app.js
@@ -4,17 +4,34 @@
  * 1. require hbs
  * 2. Set the View Engine
  * 3. app.all() with the res.render() method
- * 4. require the data
- * 5. Pass data to the res.render() method
+ * 4. set the views directory to tell express where templated are
+ * 5. require the data
+ * 6. Pass data to the res.render() method
  */
 
 const express = require('express');
 const path = require('path');
+// * 1. require hbs
+const hbs = require('hbs')
 
 const app = express();
- 
+
+// * 2. Set the View Engine
+app.set('view engine', 'hbs')
+
+// * 4. set the views directory to tell express where templated are
+app.set(path.join(__dirname, '/views'))
+
 app.all('/', (req, res)=> {
-  res.sendFile(path.join(__dirname, "./index.html"));
+  // res.sendFile(path.join(__dirname, "./index.html"));
+  // * 3. app.all() with the res.render() method
+  res.render('index', { pageTitle: "Home Page", cohortName: "Pizza Bytes" })
+})
+
+app.all('/about', (req, res)=> {
+  // res.sendFile(path.join(__dirname, "./about.html"));
+  // * 3. app.all() with the res.render() method
+  res.render('about')
 })
 
 app.listen(3000)
