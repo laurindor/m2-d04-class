965ed39e12a4f85e05c05b8ae82003ed6d87cfc0
marco-ih-bcn <marco.santonastasi@ironhack.com>
2021-06-24 09:27:43 +0200

    add more thigs to the lesson

--- 01_code-express_layout_partials/app.js
+++ 01_code-express_layout_partials/app.js
@@ -1,13 +1,12 @@
 /**
  * TOC Express dynamic views
  * 
- * 0. The problem with res.sendFile(path.join(__dirname, "file/path/index.html"))
  * 1. require hbs
  * 2. Set the View Engine
  * 3. app.all() with the res.render() method
- * 4. Pass data to the res.render() method
+ * 4. require the data
+ * 5. Pass data to the res.render() method
  */
-
 
 const express = require('express');
 const path = require('path');
@@ -17,3 +16,5 @@ const app = express();
 app.all('/', (req, res)=> {
   res.sendFile(path.join(__dirname, "./index.html"));
 })
+
+app.listen(3000)
